d632b6c1f9dc94a28381a7f4b4395e45adc73011
Brady Wright <Pro75357@gmail.com>
2017-12-12 15:25:47 -0800

    reformat for build

--- imports/global.js
+++
@@ -1,8 +0,0 @@
-// global functions go here
-
-getAge = function(dobString){
-    let dob = new Date (dobString)
-    let ageDifMs = Date.now() - dob.getTime()
-    let ageDate = new Date(ageDifMs)
-    return Math.abs(ageDate.getUTCFullYear()- 1970)
-};
