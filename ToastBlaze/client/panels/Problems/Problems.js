b603bf81c5593e52831733a81136fe7b9e08da98
Brady Wright <Pro75357@gmail.com>
2017-12-19 17:04:06 -0800

    A1c color values (not yet in order)

--- ToastBlaze/client/panels/Problems/Problems.js
+++ ToastBlaze/client/panels/Problems/Problems.js
@@ -8,25 +8,32 @@ function Pbs(metricCategory){
 
 Template.Problems.helpers({
 
-    Diabetes() {
+    Diabetes(){
         let metricCategory = 'Diabetes';
         if(Pbs(metricCategory).count()>0){
             return Pbs(metricCategory).fetch()
         }
     },
-    hasHeartFailure(){
+    A1cList(){
+        if(Obs.find({category: 'labs', name: 'A1c'}, {sort: {date: 1}}).count() > 0) {
+            return Obs.find({category: 'labs', name: 'A1c'}).fetch()
+        } else {
+            return ['None Found']
+        }
+    },
+    HeartFailure(){
         let metricCategory='Heart Failure';
         if(Pbs(metricCategory).count()>0){
             return Pbs(metricCategory).fetch()
         }
     },
-    hasHypertension(){
+    Hypertension(){
         let metricCategory='Hypertension';
         if(Pbs(metricCategory).count()>0){
             return Pbs(metricCategory).fetch()
         }
     },
-    hasHeartDisease(){
+    HeartDisease(){
         let metricCategory='Heart Disease';
         if(Pbs(metricCategory).count()>0){
             return Pbs(metricCategory).fetch()
@@ -37,9 +44,21 @@ Template.Problems.helpers({
         return null;
     },
 
+    //return a color to indicate when values are higher or lower than the reference value.
+    // This one assumes the high values are bad and low are ok.
+    colorValueHighBad(value, refLow, refHigh){
+      if (value > refHigh) {
+          return '#fc9c87'
+      } else if (value < refLow) {
+          return '#87fcea'
+      } else {
+          return '#93fc87'
+      }
+    },
+
     obsVomit(){
         if (Obs.find().count() > 0) {
-            return JSON.stringify(Obs.find().fetch(), null, 2)
+            return JSON.stringify(Obs.find({category: 'labs', name: 'A1c'}).fetch(), null, 2)
         }
     }
 });
